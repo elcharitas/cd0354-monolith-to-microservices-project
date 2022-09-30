08e8bb8d1b63c914a78a252261e4137a2b130e52
elcharitas <jonathanirhodia@gmail.com>
2022-09-30 22:47:32 +0100

    feat: use docker images + start script

---
+++ start.sh
@@ -0,0 +1,10 @@
+# run env setup script
+sh ./set_env.sh;
+# Make sure the Docker services are running in your local machine
+# Remove unused and dangling images
+docker image prune --all
+# Run this command from the directory where you have the "docker-compose-build.yml" file present
+docker-compose -f docker-compose-build.yml build --parallel
+
+# Run this command from the directory where you have the "docker-compose.yml" file present
+docker-compose up
