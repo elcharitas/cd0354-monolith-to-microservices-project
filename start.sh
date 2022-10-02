b0fe4f5334456c5fbd4e145e6a111a8f5e263cd8
elcharitas <jonathanirhodia@gmail.com>
2022-10-02 22:30:12 +0100

    feat: use env files

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # run env setup script
-sh ./set_env.sh && \
+export $(cat .env | xargs) && \
 # Make sure the Docker services are running in your local machine
 # Run this command from the directory where you have the "docker-compose-build.yml" file present
 docker-compose -f docker-compose-build.yml build --parallel && \
