d7935b0446a851a0c6ef30fb81d2063766681953
elcharitas <jonathanirhodia@gmail.com>
2022-10-02 22:44:05 +0100

    chore: patch start script with missing yaml

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 export $(cat .env | xargs) && \
 # Make sure the Docker services are running in your local machine
 # Run this command from the directory where you have the "docker-compose-build.yml" file present
-docker-compose -f docker-compose-build.yml build --parallel && \
+docker-compose -f docker-compose-build.yaml build --parallel && \
 
 # Run this command from the directory where you have the "docker-compose.yml" file present
 docker-compose up;
