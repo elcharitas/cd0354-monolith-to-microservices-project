472e5dd388c6ca8092dc6188fab5fd85fc41aa5a
elcharitas <jonathanirhodia@gmail.com>
2022-10-09 06:40:19 +0100

    feat: add k8 start script

---
+++ kube-start.sh
@@ -0,0 +1,20 @@
+# Apply env variables and secrets
+kubectl apply -f aws-secret.yaml
+kubectl apply -f env-secret.yaml
+kubectl apply -f env-configmap.yaml
+
+# api feed
+kubectl apply -f api-feed-deployment.yaml
+kubectl apply -f api-feed-service.yaml
+
+# api user
+kubectl apply -f api-user-deployment.yaml
+kubectl apply -f api-user-service.yaml
+
+# reverse proxy
+kubectl apply -f reverseproxy-deployment.yaml
+kubectl apply -f reverseproxy-service.yaml
+
+# frontend
+kubectl apply -f frontend-deployment.yaml
+kubectl apply -f frontend-service.yaml
